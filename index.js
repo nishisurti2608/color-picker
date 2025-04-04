d91630a3f3309b7ed9fb2b47e5234889444fd278
Nishi Surti <01nishisurti@gmail.com>
2025-04-04 18:21:34 -0400

    All functionalities done ✅

--- index.js
+++ index.js
@@ -1,33 +1,57 @@
-const userInputColorEl = document.getElementById("color-picker").value;
+const userInputColorEl = document.getElementById("color-picker");
 const colorTypeEl = document.getElementById("color-type");
 const resultEl = document.getElementById("result");
 const hexCodes = document.getElementById("codes");
+
 let schemes;
+
+//let set on page load
+window.addEventListener("DOMContentLoaded", () => {
+  getData(userInputColorEl.value);
+});
+
 //preventing page refresh on submit button of form
 
 document.getElementById("color-info").addEventListener("submit", function (e) {
   e.preventDefault();
+
+  //catching change on color-picker
+  userInputColorEl.addEventListener("change", getData(userInputColorEl.value));
 });
 
-// using fetchAPI getting colors
+function getData(value) {
+  // using fetchAPI getting colors
 
-fetch("https://www.thecolorapi.com/scheme?hex=${userInputColorEl}")
-  .then((response) => response?.json())
-  .then((data) => {
-    schemes = Object.keys(data?._links?.schemes);
-    addSchemes(schemes);
-    renderColour(data?.colors);
-  });
+  fetch(
+    `https://www.thecolorapi.com/scheme?hex=${value.slice(1)}&mode=${
+      colorTypeEl.value
+    }`
+  )
+    .then((response) => response?.json())
+    .then((data) => {
+      schemes = Object.keys(data?._links?.schemes);
+      addSchemes(schemes); //will load available schemes
+      renderColour(data?.colors);
+    });
+}
 
 function addSchemes(schemes) {
+  //saving current value
+  const currentValue = colorTypeEl.value;
+  colorTypeEl.innerHTML = " ";
   //Checking if scheme has some data or null
-  if (schemes.length > 0)
+  if (schemes.length > 0) {
     schemes.forEach((element) => {
       const option = document.createElement("option");
       option.text = element.charAt(0).toUpperCase() + element.slice(1);
       option.value = element;
+      //Not loosing current value
+      element === currentValue
+        ? (option.selected = true)
+        : (option.selected = false);
       colorTypeEl.appendChild(option);
     });
+  }
 }
 
 //lets display colors
@@ -36,6 +60,9 @@ function renderColour(colors) {
   if (colors.length > 0) {
     const result = document.createElement("div");
     result.id = "color-pallet";
+    //clearing prevous code
+    resultEl.innerHTML = " ";
+    hexCodes.innerHTML = " ";
 
     colors.forEach((color) => {
       result.innerHTML += `<div id="color-pallet" style="background-color:${color.hex.value}"> </div>`;
